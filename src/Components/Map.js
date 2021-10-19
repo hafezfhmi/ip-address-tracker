6c6d634ff653d057370ff8c3719ae4f2f200522e
hafezfhmi <hafezfahmi2000@gmail.com>
2021-10-19 18:45:11 +0800

    Added map from leaflet using react-leaflet'

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import styles from './map.module.css';
 
 const Map = () => {
-  return <div>hello</div>;
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      className={styles.mapContainer}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 };
 
 export default Map;
