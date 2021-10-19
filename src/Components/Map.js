a56bf889e5d2ad197bd9f97a630c1abcfa03dc6a
hafezfhmi <hafezfahmi2000@gmail.com>
2021-10-19 19:45:38 +0800

    Added custom icon to leaflet map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import styles from './map.module.css';
 
 const Map = () => {
+  let myIcon = L.icon({
+    iconUrl: '/images/icon-location.svg',
+  });
+
   return (
     <MapContainer
       center={[51.505, -0.09]}
@@ -14,7 +19,7 @@ const Map = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[51.505, -0.09]} icon={myIcon}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
